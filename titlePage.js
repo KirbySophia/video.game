5b4ba8e2f170b83089704763f5f2acb42df85c5a
KirbySophia <36045947+KirbySophia@users.noreply.github.com>
2018-05-03 11:09:03 -0400

    Update titlePage.js

--- titlePage.js
+++ titlePage.js
@@ -1,6 +1,14 @@
-background(38, 25, 25);
+function setup() {
+  createCanvas(400, 400);
+    background(38, 25, 25);
 textSize(46);
 for(var i =0; i<8; i++){
     text("POLYBIUS",i+97,43*i+22,309,111);
 
+}
+}
+
+function draw() {
+  background(220);
+	ellipse(10,10,10,10);
 }
