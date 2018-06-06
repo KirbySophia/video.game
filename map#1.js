90e5e1ebfbc642ae3fa1039450f7269a45ac5ef3
KirbySophia <36045947+KirbySophia@users.noreply.github.com>
2018-06-06 09:19:13 -0400

    Create map#1.js

---
+++ map#1.js
@@ -0,0 +1,23 @@
+//chartacter controls
+var LILLY = rect(37, 110, 14,13);
+var MovePlayerUp;
+var MovePlayerDown;
+    //map 
+rect (40,1,42,25);
+    //square
+    line(90,10,393,10);
+line(10,10,46,10);
+line(10,10,10,386);
+line(11,387,395,386);
+line(393,356,395,10);
+//start of mazs 
+line(46,10,46,40);
+line(90,10,89,26);
+line(46,42,205,42);
+line(89,25,141,25);
+line(140,26,140,11);
+line(205,25,163,25);
+line(239,25,368,25);
+line(239,25,239,66);
+line(138,66,239,66);
+
