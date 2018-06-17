55465c4f2119931217f005b00c8ee372abf6206e
EllaLiuu <36045822+EllaLiuu@users.noreply.github.com>
2018-06-17 17:29:58 -0400

    made changes
    fixed map 3

--- Scene.js
+++ Scene.js
@@ -364,28 +364,10 @@ var drawMap3 = function() {
   triangle(100, 100, 300, 100, 200, 227);
   ellipse(308, 202, 75, 69);
   ellipse(119, 202, 51, 53);
-  
-  //wall blocking function
-  var spr1;
-var spr2;
-function setup() {
-  createCanvas(400, 400);
-  spr1 = createSprite(
-    width/2, height/2, 100, 100);
-  spr1.shapeColor = color(0);
-  spr2 = createSprite(0, 0, 50, 50);
-  spr2.shapeColor = color(128);
-}
-function draw() {
-  background(50);
-  spr2.velocity.x = (mouseX-spr2.position.x)*0.2;
-  spr2.velocity.y = (mouseY-spr2.position.y)*0.2;
-  spr2.collide(spr1);
-  drawSprites();
-}
 };
-
-var drawScene8 = function() {
+ 
+  
+ var drawScene8 = function() {
   currentScene = 8;
   background(220);
   textSize(41);
